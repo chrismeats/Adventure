1772a6d199742d65903804507947f15c1cf19906
Chris Meats <cmeats@etccomputerland.com>
2014-07-23 07:31:23 -0500

    Finished MVP and first stretch

---
+++ Adventure/CustomViewController.h
@@ -0,0 +1,13 @@
+//
+//  CustomViewController.h
+//  Adventure
+//
+//  Created by ETC ComputerLand on 7/22/14.
+//  Copyright (c) 2014 cmeats. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CustomViewController : UIViewController
+
+@end
--- Adventure/ViewController.m
+++ Adventure/ViewController.m
@@ -17,13 +17,30 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
 }
 
-- (void)didReceiveMemoryWarning
+- (void) prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+
+    //UIButton *buttonPressed = sender;
+    //ViewController *vc = segue.destinationViewController;
+    //vc.TextViewCleanBoxes1.text = @"";
+    //vc.title = buttonPressed.titleLabel.text;
+
+    //NSLog(@"%@", vc.TextViewCleanBoxes1.text);
+//    NSLog(@"%@", self.textFieldName.text);
+
+    //vc.TextViewCleanBoxes1.text = [textView1 stringByReplacingOccurrencesOfString:@"Ryan" withString:@"jimBob"];
+    //vc.TextViewCleanBoxes1.text = @"test change";
 }
+
+- (IBAction)unwindAndRestart:(UIStoryboardSegue *)sender
+{
+//    ViewController *svc = sender.sourceViewController;
+//    ViewController *vc = sender.destinationViewController;
+//    vc.labelEnding.text = svc.title;
+    //NSLog(@"%@", endingButton.titleLabel.text);
+}
+
 
 @end
